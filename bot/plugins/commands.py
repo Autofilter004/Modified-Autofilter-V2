49ee091f54c76a2c72fd6564088077f2aa8d0bf6
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-10-14 19:06:40 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -124,8 +124,7 @@ Join on our channel to get movies ✅
 
                 update.from_user.first_name),
 
-        reply_markup=reply_markup,
-
+        
         parse_mode="html",
 
         reply_to_message_id=update.message_id
